142ba1fcd7793a35ec4efa5b496c639e752c4352
EmileBureau <emilebureau@hotmail.com>
2025-02-03 13:35:14 -0500

    Added SQL step to pipeline
    Added the script and its execution in the pipeline

---
+++ stage-sql/00-stage-sql/02-run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+# Add SQL script file
+install -v -m 755 files/sql_script.sql "${ROOTFS_DIR}/home/sql_script.sql"
+
+# Create database with the previous script
+on_chroot <<- EOF
+	sqlite3 dataforus.db ­< sql_script.sql
+EOF
