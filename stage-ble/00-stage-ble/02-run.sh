f6f6d3b9a4a41581e934a4e2b975d71cbea69738
Félix Giffard <technonerdzfg@gmail.com>
2025-02-19 09:02:53 -0500

    fix path

--- stage-ble/00-stage-ble/02-run.sh
+++ stage-ble/00-stage-ble/02-run.sh
@@ -8,11 +8,11 @@ EOF
 
 
 #installing files of raspi-ble
-install -v -m 755 files/raspi-ble/advertisement.py "${ROOTFS_DIR}/pi/home/raspi-ble/advertisement.py"
-install -v -m 755 files/raspi-ble/bletools.py "${ROOTFS_DIR}/pi/home/raspi-ble/bletools.py"
-install -v -m 755 files/raspi-ble/ftp_ble.py "${ROOTFS_DIR}/pi/home/raspi-ble/ftp_ble.py"
-install -v -m 755 files/raspi-ble/service.py "${ROOTFS_DIR}/pi/home/raspi-ble/service.py"
-install -v -m 755 files/raspi-ble/json_test.json "${ROOTFS_DIR}/pi/home/raspi-ble/json_test.json"
+install -v -m 755 files/raspi-ble/advertisement.py "${ROOTFS_DIR}/home/pi/raspi-ble/advertisement.py"
+install -v -m 755 files/raspi-ble/bletools.py "${ROOTFS_DIR}/home/pi/raspi-ble/bletools.py"
+install -v -m 755 files/raspi-ble/ftp_ble.py "${ROOTFS_DIR}/home/pi/raspi-ble/ftp_ble.py"
+install -v -m 755 files/raspi-ble/service.py "${ROOTFS_DIR}/home/pi/raspi-ble/service.py"
+install -v -m 755 files/raspi-ble/json_test.json "${ROOTFS_DIR}/home/pi/raspi-ble/json_test.json"
 install -v -m 655 files/main.conf "${ROOTFS_DIR}/etc/bluetooth/main.conf"
 
 
