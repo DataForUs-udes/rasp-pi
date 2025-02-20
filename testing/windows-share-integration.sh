776ef98e87ecf57e2abf951c9e3650617934aa15
Félix Giffard <technonerdzfg@gmail.com>
2025-02-20 12:10:15 -0500

    test using wsl2

--- testing/windows-share-integration.sh
+++ testing/windows-share-integration.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i 's/10\.0\.0\.2/host.docker.internal/g' /rasp-pi/stage-rndis/00-stage-rndis/files/mount-abatteuse-share.sh
+sed -i 's/10\.0\.0\.2/windows-host/g' rasp-pi/stage-rndis/00-stage-rndis/files/mount-abatteuse-share.sh
 
-source /rasp-pi/stage-rndis/00-stage-rndis/files/mount-abatteuse-share.sh
+source rasp-pi/stage-rndis/00-stage-rndis/files/mount-abatteuse-share.sh
 
 ls -lh /mnt/abatteuse
