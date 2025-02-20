3e0ac69d6b4403f836c4370eeeca5c8fb1740877
Félix Giffard <technonerdzfg@gmail.com>
2025-02-20 12:17:15 -0500

    fix path

--- testing/windows-share-integration.sh
+++ testing/windows-share-integration.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i 's/10\.0\.0\.2/windows-host/g' rasp-pi/stage-rndis/00-stage-rndis/files/mount-abatteuse-share.sh
+sed -i 's/10\.0\.0\.2/windows-host/g' stage-rndis/00-stage-rndis/files/mount-abatteuse-share.sh
 
-source rasp-pi/stage-rndis/00-stage-rndis/files/mount-abatteuse-share.sh
+source stage-rndis/00-stage-rndis/files/mount-abatteuse-share.sh
 
 ls -lh /mnt/abatteuse
